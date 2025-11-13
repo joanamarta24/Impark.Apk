e378bc9be38f3c91777d9d7d072b9370c8283f97
joanamarta <joanamartasouza@gmail.com>
2025-11-13 17:16:09 -0300

    13/11/2025

--- app/src/main/java/com/example/imparkapk/data/dao/converters/menager/UsuarioStateManager.kt
+++ app/src/main/java/com/example/imparkapk/data/dao/converters/menager/UsuarioStateManager.kt
@@ -1,10 +1,12 @@
 package com.example.imparkapk.data.dao.converters.menager
 
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import com.example.imparkapk.UiState.UsuarioUiState
 import com.example.imparktcc.model.Usuario
+import com.google.gson.Gson
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.collections.emptyList
@@ -20,5 +22,15 @@ class UsuarioStateManager (private val context: Context){
         private const val KEY_LAST_LOGIN = "last_login"
 
         //TIPOS DE USUÁRIO
+        const val USER_TYPE_CLIENTE ="CLIENTE"
+        const val USER_TYPE_DONO ="DONO"
+        const val USER_TYPE_NOME = "NOME"
     }
+    private val prefs: SharedPreferences by lazy {
+        context.getSharedPreferences(PREFS_NAME,Context.MODE_PRIVATE)
+    }
+    private val gson = Gson()
+
+    //SALVAR ESTADO DO CLIENTE
+
 }
