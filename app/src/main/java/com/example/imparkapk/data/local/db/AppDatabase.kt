43f919296c5db376bd6d4ebebae1379fe7bf7038
Arthur <arthurhenrrique1705@gmail.com>
2025-11-24 17:16:21 -0300

    Incrementando versão do banco

--- app/src/main/java/com/example/imparkapk/data/local/db/AppDatabase.kt
+++ app/src/main/java/com/example/imparkapk/data/local/db/AppDatabase.kt
@@ -35,7 +35,7 @@ import com.example.imparkapk.di.DateConverter
         AvaliacaoEntity::class,
         AcessoEntity::class
     ],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 @TypeConverters(DateConverter::class)
