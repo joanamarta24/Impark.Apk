8b643ec372762f353f11e45e3dc40420d0d35e6f
Arthur <arthurhenrrique1705@gmail.com>
2025-11-28 14:01:33 -0300

    Voltando a uma versão funcional

--- app/src/main/java/com/example/imparkapk/ui/feature/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/example/imparkapk/ui/feature/dashboard/DashboardScreen.kt
@@ -1,23 +1,21 @@
 package com.example.imparkapk.ui.feature.dashboard
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -25,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -49,8 +48,9 @@ fun DashboardScreen(
 
     Column(
         modifier = Modifier
-            .fillMaxHeight(),
-        verticalArrangement = Arrangement.spacedBy(2.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(2.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ){
         Row(
             modifier = Modifier
@@ -87,16 +87,25 @@ fun DashboardScreen(
                 )
             )
         }
+        TextField(
+            value = state.pesquisa,
+            onValueChange = { vm.onPesquisarChange(it) },
+            modifier = Modifier,
+            enabled = true,
+            readOnly = false,
+            textStyle = TextStyle(),
+            label = null
+        )
+        LazyColumn {
+            for (estacionamento in state.estacionamentos) {
+                item {
+                    CardEstacionamento(
+                        content = estacionamento
+                    )
+                }
 
-        LazyColumn(
-            modifier = Modifier.weight(1f)
-        ) {
-            items(state.estacionamentos) { estacio ->
-                Log.w("Verificação", "Estacionamento: $estacio")
-                CardEstacionamento(
-                    content = estacio
-                )
             }
         }
+
     }
 }
